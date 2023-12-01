a5f4eb3871417ea6758ba4802dc29af65f4581f5
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-12-01 22:59:50 +0100

    v2

---
+++ conf/config-sample.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Configuration
+ *
+ * Usually, the wishthis installer will create the config.php for you.
+ */
+
+namespace wishthis;
+
+/**
+ * Database
+ *
+ * Used to connect to your MySQL database.
+ */
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', '__DB_NAME__');
+define('DATABASE_USER', '__DB_USER__');
+define('DATABASE_PASSWORD', '__DB_PWD__');
+
+/**
+ * Development
+ *
+ * Keep this disabled for production sites.
+ */
+define('ENV_IS_DEV', false);
+
+/**
+ * Channels
+ *
+ * It's safe to delete this if you are self-hosting. Alternatively you can
+ * replace these branches and domains with your own.
+ */
+define(
+    'CHANNELS',
+    array(
+        array(
+            'branch' => 'stable',
+            'host'   => 'wishthis.online',
+            'label'  => __('Stable'),
+        ),
+        array(
+            'branch' => 'release-candidate',
+            'host'   => 'rc.wishthis.online',
+            'label'  => __('Release candidate'),
+        ),
+    )
+);
+
+/**
+ * plausible
+ *
+ * Whether to make calls to plausible.io.
+ */
+define('PLAUSIBLE', false);
+
+/**
+ * Miscellaneous
+ */
+define('DISABLE_USER_REGISTRATION', false);
